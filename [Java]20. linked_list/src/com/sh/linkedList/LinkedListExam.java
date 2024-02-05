68fb65868e7b2a83d8260e6eace683aae201e875
Lim Seungho <ho4788@gmail.com>
2024-02-05 09:35:13 +0900

    Add files via upload

---
+++ [Java]20. linked_list/src/com/sh/linkedList/LinkedListExam.java
@@ -0,0 +1,22 @@
+package com.kh.linkedList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExam {
+
+	public static void main(String[] args) {
+		// List 인터페이스 LinkedList 객체 활용해서 값 넣고 출력
+		// String 값 넣고 수정 삭제 출력
+		
+		// LinkedList 객체 new LinkedList 객체
+		// int 값 넣고 수정 삭제 출력
+		
+		List<String> linkedList = new LinkedList<>();
+		linkedList.add("");
+		
+		linkedList.remove();
+		
+
+	}
+
+}
